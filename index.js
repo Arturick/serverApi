48739b5c7561bcd875e9d9b52d1cb52406b9fcc8
Arturrick <asunov.artur.2007@gmail.com>
2022-09-24 12:14:11 +0300

    h

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const router = require('./routers/router');
 const cors = require('cors');
 const config = require('./config');
